34049382d20ab30b51baa9c331e1c7cd7c844e99
fab2vo <fabdevaux@gmail.com>
2021-12-28 08:52:15 +0100

    Version 16 (2.16) Update notes et comments limités à ceux de l'auteur

--- app/src/main/java/com/fdx/cookbook/AsyncCallClass.java
+++ app/src/main/java/com/fdx/cookbook/AsyncCallClass.java
@@ -230,8 +230,13 @@ class AsyncCallClass extends AsyncTask<Void, Integer, Boolean> {
             for (int i = 0; i < recipeComments.size(); i++){
                 ci=recipeComments.get(i);
                 c=new Comment(ci.getTxt(),ci.getUser(),ci.getDate());
-                if (downloadedComments.isEmpty()) {c1oc.add(c);} else {
-                if (downloadedComments.indexOf(c)==-1) c1oc.add(c);}
+                if (c.getUser().getId().toString().equals(mSession.getUser().getId().toString())){
+                    if (downloadedComments.isEmpty()) {
+                        c1oc.add(c);
+                    } else {
+                        if (downloadedComments.indexOf(c)==-1) c1oc.add(c);
+                    }
+                }
             }
         }
         //----------- boucle serveur
@@ -240,8 +245,11 @@ class AsyncCallClass extends AsyncTask<Void, Integer, Boolean> {
             for (int i = 0; i < downloadedComments.size(); i++){
                 ci=downloadedComments.get(i);
                 c=new Comment(ci.getTxt(),ci.getUser(),ci.getDate());
-                if (recipeComments.isEmpty()) {cser.add(c);} else {
-                if (recipeComments.indexOf(c)==-1) cser.add(c);}
+                if (recipeComments.isEmpty()) {
+                    cser.add(c);
+                } else {
+                    if (recipeComments.indexOf(c)==-1) cser.add(c);
+                }
             }
         }
         if (!cser.isEmpty()){
@@ -254,8 +262,11 @@ class AsyncCallClass extends AsyncTask<Void, Integer, Boolean> {
             }
         }
         if (!c1oc.isEmpty()) {
-            if (!uploadComments(r,c1oc)) {ret=false;}
-            else {ret=true;}
+            if (!uploadComments(r,c1oc)) {
+                ret=false;
+            } else {
+                ret=true;
+            }
         }
         return ret;
     }
@@ -297,13 +308,16 @@ class AsyncCallClass extends AsyncTask<Void, Integer, Boolean> {
         List<Note> c1oc= new ArrayList<>();
         Note c,ci;
         if (!recipeNotes.isEmpty()){
-            for (int i = 0; i < recipeNotes.size(); i++){
-                ci=recipeNotes.get(i);
-                c=new Note(ci.getNote(),ci.getUser(),ci.getDate());
-                if (downloadedNotes.isEmpty()) {
-                    c1oc.add(c);}
-                else {
-                    if (downloadedNotes.indexOf(c)==-1) c1oc.add(c);}
+            for (int i = 0; i < recipeNotes.size(); i++) {
+                ci = recipeNotes.get(i);
+                c = new Note(ci.getNote(), ci.getUser(), ci.getDate());
+                if (c.getUser().getId().toString().equals(mSession.getUser().getId().toString())) {
+                    if (downloadedNotes.isEmpty()) {
+                        c1oc.add(c);
+                    } else {
+                        if (downloadedNotes.indexOf(c) == -1) c1oc.add(c);
+                    }
+                }
             }
         }
         //----------- boucle serveur
