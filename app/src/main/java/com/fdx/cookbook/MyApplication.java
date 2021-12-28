a2eebf0ea44e1d087a36dd732f3746b52e7feb2c
fab2vo <fabdevaux@gmail.com>
2021-12-28 12:40:01 +0100

    Revert "Version 16 (2.16) reinitialisation version 16

--- app/src/main/java/com/fdx/cookbook/MyApplication.java
+++ app/src/main/java/com/fdx/cookbook/MyApplication.java
@@ -2,7 +2,6 @@ package com.fdx.cookbook;
 
 import android.app.Application;
 import android.content.Context;
-import android.util.Log;
 import android.widget.Toast;
 
 import org.acra.ACRA;
@@ -15,7 +14,6 @@ public class MyApplication extends Application {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
-
         CoreConfigurationBuilder builder = new CoreConfigurationBuilder(this);
         //core configuration: StringFormat.JSON or StringFormat.KEY_VALUE_LIST
         builder
@@ -29,12 +27,6 @@ public class MyApplication extends Application {
         builder.getPluginConfigurationBuilder(HttpSenderConfigurationBuilder.class)
                 .withUri("http://82.66.37.73:8085/cb/crashedreport.php")
                 .withEnabled(true);
-        /*builder.getPluginConfigurationBuilder(MailSenderConfigurationBuilder.class)
-                .withMailTo("cookbookfamily.founder@gmail.com")
-                .withReportFileName("Crash.txt")
-                .withSubject("Crash ")
-                .withBody("Rapport de crash"); */
-        ACRA.DEV_LOGGING = false;
         ACRA.init(this, builder);
     }
 }
