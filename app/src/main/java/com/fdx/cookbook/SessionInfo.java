62fe2154b2bcbdd17411e9beb91401ece7e3d45a
fab2vo <fabdevaux@gmail.com>
2024-04-05 19:07:15 +0200

    Serveur https://cookbookfamily.cloud/ 23 - 3.6

--- app/src/main/java/com/fdx/cookbook/SessionInfo.java
+++ app/src/main/java/com/fdx/cookbook/SessionInfo.java
@@ -27,7 +27,7 @@ public class SessionInfo {
     private String mDevice;
     private String mPwd;
     private static final String TAG = "CB_Session";
-    private static String URLPATH="https://cookbookfamily.000webhostapp.com/cb/";
+    private static String URLPATH="https://cookbookfamily.cloud/cb/";
     public static int CONNECT_TIMEOUT = 10000;
     public static int READ_TIMEOUT = 10000;
 
